7fb8bda8b3c7df2f31d9991e370e9a1b7f09799e
Alex Sedmoy <alexsedmoy@gmail.com>
2020-11-21 00:16:43 +0200

    lesson01 V0.1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/******************************************
+        Lesson 01
+*******************************************/
+
+// объявление переменных
+let money; 
+let income; 
+let addExpenses; 
+let deposit; 
+let mission; 
+let period;
+
+// вывод сообщения на экран в модальном окне
+alert('JS message to modal');
+
+// вывод сообщения в консоль
+console.log('JS message to console');
+
