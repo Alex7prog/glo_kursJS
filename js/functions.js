0ff351290d2cba38d6cb7121670341848cbc1cf7
Alex Sedmoy <alexsedmoy@gmail.com>
2020-11-25 20:41:32 +0200

    lesson05 V1.0

---
+++ js/functions.js
@@ -0,0 +1,82 @@
+/******************************************
+       Functions
+*******************************************/
+
+// используем строгий режим
+'use strict';
+
+// определение функций:
+
+// функция определения значения на число
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n)
+}
+// функция вывода в консоль типа переменной
+let showTypeOf = function (data) {
+  console.log(data + ': ' + typeof(data));
+}
+
+// функция запроса начальных данных - месячный доход
+function start() {
+  money = +prompt('Укажите Ваш месячный доход: ');
+
+  while (!isNumber(money)) {
+    money = +prompt('Укажите Ваш месячный доход: ');
+  }
+}
+// функция запроса у пользователя и подсчета обязательных месячных расходов
+function getExpensesMonth() {
+
+  let i = 0,
+      sum,
+      sumExpensesMonth =0,
+      expenses = '';
+  
+  do {
+
+    expenses = prompt('Введите обязательную ' + (i + 1) + ' статью расходов: ');
+    
+    if (expenses !== null) {
+      
+      sum = prompt(`Во сколько обойдутся ${expenses} ?: `);
+ 
+      while (!isNumber(sum)) {
+        sum = prompt(`Во сколько обойдутся ${expenses} ?: `);
+      }
+
+      sumExpensesMonth += +sum;
+      i++;
+    
+    }
+
+  } while (expenses !== null);
+
+  return sumExpensesMonth;
+
+}
+// функция подсчета суммы накоплений за месяц 
+function getAccumulatedMonth(income, expenses) {
+  let sumAccumulatedMonth = income - expenses;
+  return sumAccumulatedMonth;
+}
+// функция подсчета срока достижения цели
+function getTargetMonth(sumMonth, sumMission) {
+  let TargetMonth = Math.ceil(sumMission / sumMonth);
+  return TargetMonth;
+}
+// функция определения уровня доходов
+function getStatusIncome(sumDay) {
+  switch (true) {
+    case (sumDay >= 1200):
+      return 'У вас высокий уровень дохода';
+      //break;
+    case (sumDay >= 600):
+      return 'У вас средний уровень дохода';
+      //break;
+    case (sumDay >= 0):
+      return 'К сожалению у вас уровень дохода ниже среднего';
+      //break;
+    default:
+      return 'Что то пошло не так';
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,51 @@
+/******************************************
+        Lesson 04
+*******************************************/
+// используем строгий режим
+'use strict';
+
+// объявление переменных
+let money = 10000, 
+    income = 'freelance', 
+    addExpenses = 'Коммунальные платежи, Бензин, Интернет',
+    deposit = true, 
+    mission = 1000000000, 
+    period = 6,
+    expensesAmount;
+
+    
+// запросы пользователю:
+// запрос размера месячного дохода пользователя - money
+start();
+// запрос у пользователя возможных расходов
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую: ');
+// запрос у пользователя о банковском депозите
+deposit = Boolean(confirm('Есть ли у вас депозит в банке?: '));
+
+// вывод в консоль типов переменных
+showTypeOf(money); 
+showTypeOf(income); 
+showTypeOf(addExpenses); 
+
+// запрос у пользователя сумм обязательных расходов и подсчет их суммы
+expensesAmount = getExpensesMonth();
+// расходы за месяц
+console.log('расходы за месяц:', expensesAmount);
+// возможные расходы в виде массива
+console.log('возможные расходы:', addExpenses.split(', '));
+
+// чистый доход
+let accumulatedMonth = getAccumulatedMonth(money, expensesAmount);
+
+// срок достижения цели в месяцах
+// getTargetMonth вернет отрицательно значение если accumulatedMonth < 0
+accumulatedMonth > 0 ? 
+  console.log('срок достижения цели:', getTargetMonth(accumulatedMonth, mission), 'месяцев')
+  : console.log('цель не будет достигнута');
+
+// бюджет на день
+let budgetDay = Math.floor(accumulatedMonth / 30);
+console.log('бюджет на день:', budgetDay);
+
+// определение уровня доходов
+console.log('уровень доходов:', getStatusIncome(budgetDay));
