b105cd9947ed28b6552dc128798fbdae27f23863
Alex Sedmoy <alexsedmoy@gmail.com>
2020-12-17 10:41:58 +0200

    lesson18 /add function call countTimer

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 /******************************************
-        Lesson 18
+        			Lesson 18
 *******************************************/
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
 	'use strict';
 
 	// Timer
 	function countTimer(deadline) {
 		const timerHours = document.querySelector('#timer-hours'),
-				timerMinutes  = document.querySelector('#timer-minutes'),
-				timerSeconds = document.querySelector('#timer-seconds');
-		
+			timerMinutes  = document.querySelector('#timer-minutes'),
+			timerSeconds = document.querySelector('#timer-seconds');
+
 		function format2Number(num) {
 			num < 10 ? num = '0' + num : num = num.toString();
 			return num;
 		}
 
 		function getTimeRemaining() {
-			let	dateStop = new Date(deadline).getTime(),
-					dateNow = new Date().getTime(),
-					timeRemaining = (dateStop - dateNow) / 1000,
-					seconds = Math.floor(timeRemaining % 60),
-					minutes = Math.floor((timeRemaining / 60) % 60),
-					hours = Math.floor(timeRemaining / 60 / 60);
+			const	dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 60 / 60);
 
-			return {timeRemaining, hours, minutes, seconds};
+			return { timeRemaining, hours, minutes, seconds };
 		}
 
 		function updateClock() {
-			let timer = getTimeRemaining();
+			const timer = getTimeRemaining();
 
 			if (timer.timeRemaining >= 0) {
 				timerHours.textContent = format2Number(timer.hours);
@@ -41,22 +41,16 @@ window.addEventListener('DOMContentLoaded', function() {
 				clearInterval(idInterval);
 			}
 		}
-		
-		//1-й вариант
-		//updateClock();
 
-		// 2-й вариант
-		const idInterval = setInterval(updateClock, 1000);
+		updateClock();
 
 	}
 
+	const timeDeadline = '17 december 2020';
 
-	// 1-й вариант
-	//const idInterval = setInterval(countTimer, 1000, '20 december 2020');
+	countTimer(timeDeadline);
 
-	// 2-й вариант
-	countTimer('17 december 2020');
 
+	const idInterval = setInterval(countTimer, 1000, timeDeadline);
 
 });
-
